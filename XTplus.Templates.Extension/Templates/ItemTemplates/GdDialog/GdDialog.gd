126f3fc0497d4bff923eca378361cd9e06d5b0c5
IInspectable <github@smjh.de>
2017-08-22 10:58:28 +0200

    GD Form, GD Dialog, GD User Control + Cosmetics

---
+++ XTplus.Templates.Extension/Templates/ItemTemplates/GdDialog/GdDialog.gd
@@ -0,0 +1,74 @@
+﻿NAMESPACE $rootnamespace$.GUI
+    DIALOG $safeitemname$
+        
+        PROPERTIES
+            Title = "$safeitemname$"
+        END PROPERTIES
+        HOTKEYS
+            HOTKEY Escape += OnEscapeAbbrechen
+        END HOTKEYS
+        CONTROLS
+
+            PANEL DialogContent Template = Content
+                LAYOUT Grid
+                    ColumnCount = 1
+                    RowCount    = 1
+
+                    RowInfos[1].Mode  = Variable // Fixed
+                    RowInfos[1].Value = 100
+                    
+                    ColumnInfos[1].Mode  = Variable // Fixed
+                    ColumnInfos[1].Value = 100
+
+                END LAYOUT
+
+                CONTROLS
+
+                    // TODO Insert Content Controls
+
+                END CONTROLS
+
+            END PANEL DialogContent
+            
+            BARMANAGER Footer
+                CONTROLS
+                
+                  CONTROL FunctionButton OK
+                        PROPERTIES
+                            Text = "OK\n"
+                            Style = StandardButton
+                            HotKeyText = "F12"
+                            Align = Right 
+                            DefaultButton = Accept
+                        END PROPERTIES
+                        EVENTS
+                            Click += OK
+                        END EVENTS
+                        HOTKEYS
+                            HOTKEY F12 += OK
+                        END HOTKEYS
+                    END CONTROL OK
+
+                    CONTROL FunctionButton Abbrechen
+                        PROPERTIES
+                            Text = "Abbrechen\n"
+                            Style = StandardButton
+                            HotKeyText = "Escape"
+                            Align = Right
+                            DefaultButton = Cancel
+                        END PROPERTIES
+                        EVENTS
+                            Click += Abbrechen
+                        END EVENTS
+                        HOTKEYS
+                            HOTKEY Escape += Abbrechen
+                        END HOTKEYS
+                    END CONTROL Abbrechen
+
+                END CONTROLS
+            END BARMANAGER Footer
+        END CONTROLS
+
+    END DIALOG $safeitemname$
+
+END NAMESPACE
---
+++ XTplus.Templates.Extension/Templates/ItemTemplates/GdForm/GdForm.gd
@@ -0,0 +1,57 @@
+﻿NAMESPACE $rootnamespace$.GUI
+    FORM $safeitemname$
+
+        PROPERTIES
+            Title = "$safeitemname$"
+        END PROPERTIES
+        HOTKEYS
+            HOTKEY Escape += OnEscapeAbbrechen 
+        END HOTKEYS
+        CONTROLS
+
+            PANEL FormContent Template = Content
+                LAYOUT Grid
+                    ColumnCount = 1
+                    RowCount    = 1
+
+                    RowInfos[1].Mode  = Variable // Fixed
+                    RowInfos[1].Value = 100
+                    
+                    ColumnInfos[1].Mode  = Variable // Fixed
+                    ColumnInfos[1].Value = 100 
+
+                END LAYOUT
+
+                CONTROLS
+
+                    // TODO Insert Content Controls
+                    
+                END CONTROLS
+
+            END PANEL FormContent
+
+            BARMANAGER Footer
+                CONTROLS
+
+                    CONTROL FunctionButton F12Button
+                        PROPERTIES
+                            Text       = "Übernehmen"
+                            Style      = StandardButton
+                            HotKeyText = "F12"
+                            Position   = 12
+                        END PROPERTIES
+                        EVENTS
+                            Click     += OnF12Übernehmen
+                        END EVENTS
+                        HOTKEYS
+                            HOTKEY F12 += OnF12Übernehmen
+                        END HOTKEYS
+                    END CONTROL F12Button
+
+                END CONTROLS
+            END BARMANAGER Footer
+        END CONTROLS
+
+    END FORM $safeitemname$
+
+END NAMESPACE
