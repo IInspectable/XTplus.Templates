569351a8e4d2f736789894dd551e10d0fe06b17b
IInspectable <github@smjh.de>
2018-07-03 20:45:55 +0200

    Nuget package update

--- Build.bat
+++ Build.bat
@@ -5,6 +5,7 @@ if "%config%" == "" (
    set config=Debug
 )
 
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" /t:restore
 "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" XTplus.Templates.sln /p:Configuration="%config%" /maxcpucount:3 /v:n
 
 REM Pause
